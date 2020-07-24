993682a02b885d1b8f6230f99bda71ada8345a24
Conchetta Park <conchettapark@MacBook-Pro.local>
2020-07-23 21:44:49 -0400

    generator updates

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
-var fs = require("fs");
-var inquirer = require("inquirer");
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "project",
+    message: "What is your project name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What type of license should your project have?",
+    choices: [],
+  },
+  {
+    type: "input",
+    name: "dependencies",
+    message: "What command should be run to install dependencies?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be used to run test?",
+  },
+  {
+    type: "input",
+    name: "repo",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What does the user need to know about contributing to the repo?",
+  },
+];
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+});
+// .catch((error) => {
+//   if (error) {
+//     console.log("try again");
+//     console.log(error);
+// } else {
+//   // Something else when wrong
+//   }
+// });
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile("README.txt", questions, function (err) {});
+}
 
 // function to initialize program
 function init() {}
